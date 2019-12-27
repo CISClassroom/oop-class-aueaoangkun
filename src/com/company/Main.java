db30ab7416a1f33bb9c45bcebee9105faca63403
unknown <aueaoangkun@kkumail.com>
2019-12-27 22:22:39 +0700

    ส่งการบ้านนะครับ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,36 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Student mark = new Student();
-        Student tontan = new Student();
-        Student dream = new Student();
+        Student FinLand = new Student();
+        FinLand.name = "นายเอื้ออังกูร สร้อยอุดม";
+        FinLand.id =  "623410059-1";
+        FinLand.major = "วิทยาการคอมพิวเตอร์";
 
-        mark.name = "X mark";
-        mark.id =  "0001";
-        mark.major = "CIS";
-        tontan.name = "Ton Tan";
-        tontan.id =  "0002";
-        tontan.major = "CIS";
-        dream.name = "Dream all night";
-        dream.id = "002";
-        dream.major = "CS";
-
-        System.out.println("1. " + mark.name + " " + mark.major);
-        System.out.println("2. " + tontan.name + " " + tontan.major);
-
-        Student[] oopclass = {mark, tontan,dream};
-
-        System.out.println("1. " + oopclass[0].name);
-        System.out.println("2. " + oopclass[1].name);
+        System.out.println("ชื่อ " + FinLand.name +"\nรหัสนักศึกษา " + FinLand.id + "\nสาขา " + FinLand.major );
     }
-    public  void find_CIS_Student(Student[] student){
-        for(int i = 0 ; i < student.length; i++){
-            if (student[i].major == "CIS"){
-                System.out.println((i+1 + ". " + student[i].name));
-            }
-        }
-
-    }
-
 }
